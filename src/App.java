416dccaf101bf0d78dae03a5a1b76b0c7960850e
Saif Chouaya <chouayasaif@gmail.com>
2024-09-26 18:05:59 +0200

    initial commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 360; /*  we have chosen those numbers because they are dimensiosn of our images*/
+        int boardheight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
